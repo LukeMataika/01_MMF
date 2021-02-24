4bc48a8b8ce857630f9f63d565c7be4e648dcb56
LukeMataika <78513611+LukeMataika@users.noreply.github.com>
2021-02-24 14:43:16 +1300

    Update 00_MMF_base_v1.py

--- 00_MMF_base_v1.py
+++ 00_MMF_base_v1.py
@@ -25,6 +25,37 @@ def not_blank(question):
 # ask user if they have used the program before & show instructions if necessary
 
 # loop to get ticket details
+        # start of loop
+
+        # initiate loop so that it runs at least once
+        name = ""
+        count = 0
+        MAX_TICKETS = 5
+
+        while name != "xxx" and count < MAX_TICKETS:
+
+            # tells users how many seats are left
+            if count < 4:
+                print("you have {} seats "
+                      "left".format(MAX_TICKETS - count))
+
+                # warns user that only one seat is left
+        else:
+            print("*** There is ONE seat left!! ***")
+
+            # GET DETAILS...
+
+            # get name (cant be blank
+            name = not_blank ("name: ")
+            count += 1
+            print()
+
+        if count == MAX_TICKETS:
+            print("you have solid all available tickets!")
+        else:
+            print(" you have sold {} tickets.  \n"
+                  "there are {} places still available"
+                  .format(count, MAX_TICKETS - count))
 
     # get name (cant be blank)
     name = not_blank("name: ")
